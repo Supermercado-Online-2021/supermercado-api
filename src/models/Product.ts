e2e5b96c582279c218d44ecb99d01774b65f8f9e
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-09-15 18:03:52 -0300

    create product model

---
+++ src/models/Product.ts
@@ -0,0 +1,49 @@
+
+import { DataTypes } from 'sequelize';
+import sequelize from "@Database/index";
+
+
+
+const Product = sequelize.define( 'Product', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    nome: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    preco: {
+        type: DataTypes.DECIMAL(4,2),
+        allowNull: false
+    },
+    quantidade: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    image_src: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    codigo_barras: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'categories',
+            key: 'id'
+        }
+    },
+}, {
+    tableName: 'products',
+    timestamps: false
+});
+
+
+
+export default Product;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,16 @@
 
 import User from '@Models/User'
 import Category from '@Models/Category'
+import Product from './Product'
+
+
+
+Product.belongsTo( Category, { foreignKey: 'category_id' } );
 
 
 
 export default {
     User,
-    Category
+    Category,
+    Product
 }
