055063e27160ca5db146ee07b0aea47db08db35f
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-09-15 18:04:53 -0300

    add swagger docs

--- index.ts
+++ index.ts
@@ -2,19 +2,20 @@
 require('dotenv').config();
 
 import express from 'express';
-
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+import swaggerDocument from './swagger.json';
 
 import routes from '@Routes/index'
 
 
 
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.use( '/api', routes);
+app.use( '/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
