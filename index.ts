01bb85cc5c238ed1eb51e5b22a6c928f8df60e83
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-09-23 17:36:25 -0300

    add not found

--- index.ts
+++ index.ts
@@ -1,23 +1,31 @@
 
 require('dotenv').config();
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 const swaggerUi = require('swagger-ui-express');
 import swaggerDocument from './swagger.json';
 
-import routes from './src/routes/'
+import routes from './src/routes/';
 
 
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use( '/api', routes);
-app.use( '/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use( '/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customSiteTitle: 'Supermercado Online API'}));
+
+app.get( '*', ( req: Request, res: Response ) => {
+    return res.status(404).json({
+        error: 'Page not found',
+        message: "Página não encontrada, consulte a documentação da api.",
+        documentation: '/api/docs'
+    });
+}); 
 
 
 
