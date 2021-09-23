bcf8182bccd92405d6d6c310d8a94ae4dd7843cf
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-09-23 17:37:34 -0300

    validate password

--- src/controllers/userController/insertUser.ts
+++ src/controllers/userController/insertUser.ts
@@ -2,11 +2,23 @@
 import { Request, Response } from 'express'
 import models from '../../models/'
 
+import validatePassword from '../../util/validatePassword'
+
 
 
 async function insertUser( req: Request, res: Response ) {
     try {
-        const result = await models.User.create(req.body);
+        const { email, senha, cpf } = req.body;
+        const { nome, sobrenome, telefone, data_nascimento } = req.body;
+
+
+        const validationPassword = validatePassword(senha);
+        if( validationPassword.pass === false )
+            return res.status(400).json({
+                error: validationPassword.message
+            });
+
+        const result = await models.User.create(req.body); 
 
         return res.status(201).json(result);
     } catch(err) {
