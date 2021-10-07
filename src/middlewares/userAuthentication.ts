9763e0bae8a231098c9a53aa72778a70ba601191
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-10-07 01:18:05 -0300

    auth user middleware

---
+++ src/middlewares/userAuthentication.ts
@@ -0,0 +1,60 @@
+
+import { Request, Response, NextFunction } from 'express';
+
+import { verify } from '../util/authentication';
+import models from './../models';
+
+
+
+interface UserToken {
+    id: number,
+    email: string
+}
+
+
+
+async function userAuthMiddleware( req: Request, res: Response, next: NextFunction ) {
+    try {
+        const { token } = req.headers;
+        
+        const { id } = await verify<UserToken>( String(token) );
+        const user = await models.User.findByPk(id);
+
+        res.locals = {
+            ...res.locals,
+            auth: true,
+            user
+        };
+
+        return next();
+    } catch(err) {
+        return res.status(401).json({
+            auth: false, 
+            err
+        });
+    } 
+}
+
+export async function opcionalAuthUserMiddleware( req: Request, res: Response, next: NextFunction ) {
+    try {
+        const { token } = req.headers;
+
+        const { id } = await verify<UserToken>( String(token) );
+        const user = await models.User.findByPk(id);
+
+        res.locals = {
+            ...res.locals,
+            auth: true,
+            user
+        };
+    } catch(err) {
+        res.locals = {
+            ...res.locals,
+            auth: false,
+        };
+    } 
+
+    return next();
+}
+
+export default userAuthMiddleware;
