14e1f1f88099bb80e1ee2b039c0a9ebb0b1297e5
silasmelo17 <silasmelo1967@gmail.com>
2021-10-11 15:59:12 -0300

    add sqlite config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,14 +1,19 @@
 
 module.exports = {
-    development: {
-        host: process.env.DEV_HOST,
-        port: process.env.DEV_DB_PORT,
-        database: process.env.DEV_DATABASE,
-        username: process.env.DEV_USERNAME,
-        password: process.env.DEV_PASSWORD,
-        dialect: process.env.DEV_DIALECT,
-        logging: false
-    },
+    development: process.env.DEV_DIALECT === 'sqlite' 
+        ? {
+            dialect: process.env.DEV_DIALECT,
+            storage: process.env.DEV_STORAGE
+        }
+        : {
+            host: process.env.DEV_HOST,
+            port: process.env.DEV_DB_PORT,
+            database: process.env.DEV_DATABASE,
+            username: process.env.DEV_USERNAME,
+            password: process.env.DEV_PASSWORD,
+            dialect: process.env.DEV_DIALECT,
+            logging: false
+        },
     production: {
         host: process.env.PROD_HOST,
         port: process.env.PROD_DB_PORT,
