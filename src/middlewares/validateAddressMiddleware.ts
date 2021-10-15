10a6a234d99a3f643c62d5d54e1cbf6a22182bb2
silasmelo17 <silasmelo1967@gmail.com>
2021-10-15 16:45:53 -0300

    validate address cep

---
+++ src/middlewares/validateAddressMiddleware.ts
@@ -0,0 +1,45 @@
+
+import { NextFunction, Request, Response } from "express";
+import axios, { AxiosResponse } from 'axios';
+
+import validationCPF from '../middlewares/userValidation/validateCPF';
+
+
+
+export interface ViaCepResult {
+    logradouro: "Rua Ernest Renam",
+    bairro: "Paraisópolis",
+    localidade: "São Paulo",
+    uf: "SP",
+    erro?: boolean
+}
+
+async function validationCEP (cep: string): Promise<boolean> {
+    try{
+        const url = `https://viacep.com.br/ws/${cep}/json/`
+        const address = await axios
+            .get<any, AxiosResponse<ViaCepResult>>(url);
+
+        return address.data.erro ? false: true;
+    }catch(err) {
+        return false;
+    }
+}
+
+
+
+async function validateAddressMiddleware( req: Request, res: Response, next: NextFunction ) {
+    const { cpf, cep } = req.body;
+
+    const validateCEP = validationCEP(cep);
+    const validateCPF = validationCPF(cpf);
+
+    if(!validateCEP)
+        return res.status(400).json({ message: "CEP inválido "});
+    if(!validateCPF.pass)
+        return res.status(400).json(validateCPF.message);
+
+    next();
+}
+
+export default validateAddressMiddleware;
