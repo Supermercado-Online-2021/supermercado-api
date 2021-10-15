85fb19b511a4019daab27d8e52374af4276680fe
silasmelo17 <silasmelo1967@gmail.com>
2021-10-15 16:44:38 -0300

    create table addresses

---
+++ src/database/migrations/20211015105830-create_table_addresses.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('addresses', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            cep: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            number: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            complement: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            references: {
+                type: Sequelize.STRING,
+                allowNull: true
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                }
+            }
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('addresses'); 
+    }
+};
