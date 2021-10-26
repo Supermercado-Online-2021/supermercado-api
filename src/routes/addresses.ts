235ce5dd8c4aa1ee48cb4b199672f3b0ac34018b
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-10-25 23:10:30 -0300

    finish router addresses

--- src/routes/addresses.ts
+++ src/routes/addresses.ts
@@ -1,6 +1,6 @@
 
 import express from 'express';
-// import validateAddressMiddleware from 'src/middlewares/validateAddressMiddleware';
+import validateAddressMiddleware from '../middlewares/validateAddressMiddleware';
 import addressController from '../controllers/addressController/';
 import userAuthMiddleware from '../middlewares/userAuthentication';
 
@@ -8,10 +8,12 @@ import userAuthMiddleware from '../middlewares/userAuthentication';
 
 const router = express.Router();
 
-router.post( '/addresses', userAuthMiddleware, addressController.insertAddress );
 router.get( '/addresses', userAuthMiddleware, addressController.findAllAddresses );
 router.get( '/addresses/:id', userAuthMiddleware, addressController.findOne );
-router.put( '/addresses/:id', userAuthMiddleware, addressController.updateAddress );
+
+router.post( '/addresses', userAuthMiddleware, validateAddressMiddleware, addressController.insertAddress );
+router.put( '/addresses/:id', userAuthMiddleware, validateAddressMiddleware, addressController.updateAddress );
+
 router.delete( '/addresses/:id', userAuthMiddleware, addressController.removeAddress );
 
 
