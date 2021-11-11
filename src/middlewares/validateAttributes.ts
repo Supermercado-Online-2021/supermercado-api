9e9f51f312a999e50b0981845ae3b448fe90297b
silasmelo17 <silasmelo1967@gmail.com>
2021-11-10 23:40:27 -0300

    update attribute id

--- src/middlewares/validateAttributes.ts
+++ src/middlewares/validateAttributes.ts
@@ -9,30 +9,30 @@ import models from '../models/';
 const validateFields = (query: Query): string[] => {
     const { fields } = query;
 
-    return fields 
+    return fields
         ? typeof fields === 'string'
             ? fields.split(',')
             : fields instanceof Array
-                ? fields.map( value => String(value))
+                ? fields.map(value => String(value))
                 : []
         : [];
 }
 
 const validateAttributes = (fields: string[], allowedAttributes: string[]): string[] => {
-    const arr = fields 
-        ? fields.filter( field => allowedAttributes.some( attr => attr === field ))
+    const arr = fields
+        ? fields.filter(field => allowedAttributes.some(attr => attr === field))
         : [];
 
-    return arr.length > 0 
+    return arr.length > 0
         ? arr
         : allowedAttributes;
 }
 
-const validateQuery = (allowedAttributes: string[]): RequestHandler => 
-    async ( req: Request, res: Response, next: NextFunction ) => {
+const validateQuery = (allowedAttributes: string[]): RequestHandler =>
+    async (req: Request, res: Response, next: NextFunction) => {
 
         try {
-            const fields = validateFields( req.query );
+            const fields = validateFields(req.query);
             const attributes = validateAttributes(fields, allowedAttributes);
 
             res.locals = {
@@ -40,20 +40,29 @@ const validateQuery = (allowedAttributes: string[]): RequestHandler =>
                 fields,
                 attributes
             };
-        
+
             return next();
-        } catch(err) {
+        } catch (err) {
             return res.status(503).json(err);
         }
     }
 
 
 
+const productAttributes = (() => {
+    const [ _, ...attributes] = Object.keys(models.Product.rawAttributes);
+    return attributes;
+})();
 
-const productAttributes = Object.keys(models.Product.rawAttributes);
-const userAttributes = Object.keys(models.User.rawAttributes);
-const cardAttributes = Object.keys(models.Card.rawAttributes);
+const userAttributes = (() => {
+    const [ _, ...attributes] = Object.keys(models.User.rawAttributes);
+    return attributes;
+})();;
 
+const cardAttributes = (() => {
+    const [ _, ...attributes] = Object.keys(models.Card.rawAttributes);
+    return attributes;
+})();
 
 export const validateProductAttributes = validateQuery([...productAttributes]);
 export const validateUserAttributes = validateQuery([...userAttributes]);
