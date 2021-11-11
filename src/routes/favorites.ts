edba0920606f40d3de47c7b37957eb1310cf353f
silasmelo17 <silasmelo1967@gmail.com>
2021-11-10 23:35:46 -0300

    add middleware include product

--- src/routes/favorites.ts
+++ src/routes/favorites.ts
@@ -4,13 +4,20 @@ import express from 'express';
 import favoriteController from '../controllers/favoriteController';
 import userAuthMiddleware from '../middlewares/userAuthentication';
 import validatePagination from '../middlewares/validatePagination';
+import userProductIncludeMiddleware from '../middlewares/userProductIncludeMiddleware';
 
 
 
 const router = express.Router();
 
 router.post( '/favorites/product/:product_id', userAuthMiddleware, favoriteController.toggleProductFavorites );
-router.get( '/favorites/user', userAuthMiddleware, validatePagination, favoriteController.findAllFavorites );
+
+router.get( '/favorites/user', 
+    userAuthMiddleware, 
+    validatePagination, 
+    userProductIncludeMiddleware, 
+    favoriteController.findAllFavorites 
+);
 
 
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,17 +4,38 @@ import userValidationMiddleware from '../middlewares/userValidation';
 import userAuthMiddleware, { opcionalAuthUserMiddleware } from '../middlewares/userAuthentication';
 
 import express from 'express'
+import userAlreadyExistsMiddleware from '../middlewares/userAlreadyExistsMiddleware';
 
 
 
 const router = express.Router();
 
-router.post( '/user/register', userValidationMiddleware, userController.userInsert );
-router.post( '/user/signout', userAuthMiddleware, userController.signOutUser );
-router.post( '/user/signin', userController.signInUser );
-router.post( '/user/logged', opcionalAuthUserMiddleware, ( _, res ) => { 
-    return res.status(200).json(res.locals) 
-})
+router.post('/user/register',
+    userAlreadyExistsMiddleware,
+    userValidationMiddleware,
+    userController.userInsert
+);
+
+router.put('/user/update',
+    userAlreadyExistsMiddleware,
+    userController.userUpdate
+);
+
+router.post('/user/signout',
+    userAuthMiddleware,
+    userController.signOutUser
+);
+
+router.post( '/user/refresh', 
+    userAuthMiddleware, 
+    userController.refreshToken 
+);
+
+router.post('/user/signin', userController.signInUser);
+
+router.post('/user/logged', opcionalAuthUserMiddleware, (_, res) => {
+    return res.status(200).json(res.locals)
+});
 
 
 
