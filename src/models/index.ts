11d4044e0ba14997f337efe3f470f9c2326d74e5
silasmelo17 <silasmelo1967@gmail.com>
2021-11-10 23:28:50 -0300

    add models

--- src/models/index.ts
+++ src/models/index.ts
@@ -11,31 +11,60 @@ import Card from './Card';
 
 import Cart from './Cart';
 
+import Status from './Status';
+import Order from './Order';
+import OrderStatus from './OrderStatus';
+
+import PaymentMethod from './PaymentMethod';
+import ShoppingList from './ShoppingList';
+
 
 
 Product.belongsTo( Category, { foreignKey: 'category_id' } );
 Category.hasMany( Product, { foreignKey: 'id' } );
 
 Favorite.belongsTo( User, { foreignKey: 'user_id' });
-User.hasMany( Favorite, { foreignKey: 'user_id' } );
+User.hasMany( Favorite, { foreignKey: 'user_id' });
 
 Favorite.belongsTo( Product, { foreignKey: 'product_id' });
-Product.hasMany( Favorite, { foreignKey: 'product_id' } );
+Product.hasMany( Favorite, { foreignKey: 'product_id' });
 
-Card.belongsTo( CardTypes, { foreignKey: 'type_id' } );
+Card.belongsTo( CardTypes, { foreignKey: 'type_id' });
 CardTypes.hasMany(Card, { foreignKey: 'type_id' });
 
-Card.belongsTo( User, { foreignKey: 'user_id '} );
+Card.belongsTo( User, { foreignKey: 'user_id '});
 User.hasMany(Card, { foreignKey: 'user_id' });
 
-Cart.belongsTo( User, { foreignKey: 'user_id'} );
+Cart.belongsTo( User, { foreignKey: 'user_id'});
 User.hasMany(Cart, { foreignKey: 'user_id' });
 
-Cart.belongsTo( Product, { foreignKey: 'product_id' } );
+Cart.belongsTo( Product, { foreignKey: 'product_id' });
 Product.hasMany( Cart, { foreignKey: 'product_id' });
 
-Address.belongsTo( User, { foreignKey: 'user_id' } );
-User.hasMany( Address, { foreignKey: 'id' } );
+Address.belongsTo( User, { foreignKey: 'user_id' });
+User.hasMany( Address, { foreignKey: 'id' });
+
+
+
+Order.belongsToMany( Status, { through: OrderStatus });
+Status.belongsToMany( Order, { through: OrderStatus });
+
+PaymentMethod.belongsTo( Order, { foreignKey: 'form_payment_id' });
+Order.hasOne( PaymentMethod, { foreignKey: 'form_payment_id' });
+
+Address.belongsTo( Order, { foreignKey: 'address_id' });
+Order.hasOne( Address,{ foreignKey: 'address_id' });
+
+Order.hasOne( User, { foreignKey: 'user_id' });
+User.belongsTo( Order, { foreignKey: 'user_id' });
+
+
+
+ShoppingList.hasOne( Order, { foreignKey: 'order_id' });
+Order.belongsTo( ShoppingList, { foreignKey: 'order_id' });
+
+Product.hasMany( ShoppingList, { foreignKey: 'product_id' });
+ShoppingList.belongsTo( Product, { foreignKey: 'product_id' });
 
 
 
@@ -43,12 +72,14 @@ export default {
     User,
     Address,
     Favorite,
-
     Category,
     Product,
-
     CardTypes,
     Card,
-
-    Cart
-}
+    Cart,
+    Order,
+    OrderStatus,
+    Status,
+    ShoppingList,
+    PaymentMethod
+};
