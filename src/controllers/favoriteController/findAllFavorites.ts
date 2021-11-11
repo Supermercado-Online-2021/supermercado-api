dcad8bbdeff99bc26d26891fb3d1a6840b146d45
silasmelo17 <silasmelo1967@gmail.com>
2021-11-10 23:35:07 -0300

    add include product

--- src/controllers/favoriteController/findAllFavorites.ts
+++ src/controllers/favoriteController/findAllFavorites.ts
@@ -7,15 +7,20 @@ import models from '../../models';
 
 async function findAllFavorites( req: Request, res: Response ) {
     try {
-        const { limit, offset, page } = res.locals;
+        const { limit, offset, page, include } = res.locals;
         const { id } = res.locals.user;
 
         const { rows: data, count } = await models.Favorite.findAndCountAll({
             where: { user_id: id },
             limit, offset,
             include: {
-                model: models.Product
-            }
+                model: models.Product,
+                include: [
+                    ...include
+                ]
+            },
+            raw: true,
+            nest: true
         });
 
         return res.status(200).json({
